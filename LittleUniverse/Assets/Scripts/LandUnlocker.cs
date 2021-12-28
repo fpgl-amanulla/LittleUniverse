fa23b92f147cfc7043d56fdb6a7497c25171fa86
fpgl-amanulla <mdamanulla44@gmail.com>
2021-12-28 19:25:38 +0600

    Land Unlock mechanism.

--- LittleUniverse/Assets/Scripts/LandUnlocker.cs
+++ LittleUniverse/Assets/Scripts/LandUnlocker.cs
@@ -7,6 +7,7 @@ using Random = UnityEngine.Random;
 
 public class LandUnlocker : MonoBehaviour
 {
+    //public GameObject unlockEffect;
     public GameObject landToUnlock;
     public Transform collectableJumpTf;
     public int unlockResAmount = 20;
@@ -22,26 +23,35 @@ public class LandUnlocker : MonoBehaviour
 
         if (canUnlock)
         {
-            canUnlock = false;
-            FlyCollectableAnim();
+            //canUnlock = false;
+            StartCoroutine(FlyCollectableAnim());
         }
     }
 
-    private void FlyCollectableAnim()
+    private IEnumerator FlyCollectableAnim()
     {
-        GameObject collectableIns = Instantiate(collectablePrefab, player.transform.position, Quaternion.identity);
-        collectableIns.transform.DOJump(collectableJumpTf.position, 3, 1, .25f).OnComplete(delegate
+        for (int i = 0; i < Random.Range(2, 4); i++)
         {
-            canUnlock = true;
-            Destroy(collectableIns);
-            unlockResAmount--;
-            if (unlockResAmount <= 0)
-            {
-                //Land Unlocked
-                landToUnlock.SetActive(true);
-                this.gameObject.SetActive(false);
-            }
-        });
+            GameObject collectableIns = Instantiate(collectablePrefab, player.transform.position, Quaternion.identity);
+            Vector3 pos = Random.insideUnitSphere + (Vector3.up * 4) + transform.position;
+            collectableIns.transform.DOMove(pos, .25f).OnComplete(delegate
+                   {
 
+                       collectableIns.transform.DOMove(collectableJumpTf.position, .5f).OnComplete(delegate
+                        {
+                            Destroy(collectableIns);
+                            unlockResAmount--;
+                            //canUnlock = true;
+                            if (unlockResAmount <= 0)
+                            {
+                                //Land Unlocked
+                                //Instantiate(unlockEffect, landToUnlock.transform.localPosition, Quaternion.identity);
+                                landToUnlock.SetActive(true);
+                                this.gameObject.SetActive(false);
+                            }
+                        });
+                   });
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
